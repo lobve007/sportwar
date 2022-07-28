b99ec51abeddea7b2ea3b9c1a8ed60b6b0efc574
lobve007 <lobve007@gmail.com>
2022-07-28 15:43:32 +0800

    提交

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,6 @@ export default function PageFooter() {
     <footer className={styles.footer}>
       <h3>CONTACT US</h3>
       <ul>
-        {/* <li className={styles.email}><a href="mailto:contact@sportswar.com?subject=这是邮件标题&body=这是邮件内容"> </a></li> */}
         <li className={styles.email}><a onClick={() => { setComingShow(true); copy('contact@sportswar.com') }}> </a></li>
         <li className={styles.twitter}><a href="https://twitter.com/Sportswar_real" target="_blank"> </a></li>
         <li className={styles.miao}><a href="https://discord.gg/sportswar" target="_blank"> </a></li>
